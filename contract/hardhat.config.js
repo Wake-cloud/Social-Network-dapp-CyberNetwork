f84affe34ad6320e71b1308b7f6616bbc34082ca
Theodoros Moschoglou <146652642+Wake-cloud@users.noreply.github.com>
2024-11-24 08:43:38 +0200

    Update hardhat.config.js

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -5,11 +5,11 @@ module.exports = {
   networks: {
     polygonAmoy: {
       url: "https://polygon-amoy.g.alchemy.com/v2/bXVsZXhsIcgy7ceLe92s7v8vZsQG64hB",  // RPC URL for Mumbai Testnet
-      accounts: ["a5f1f59de420690d57900787a7bd61645479b3eed7f285384873d2a96da19555"],  // Hardcode your private key here
+      accounts: [""],  // Hardcode your private key here
     },
     polygonMainnet: {
       url: "https://polygon-mainnet.g.alchemy.com/v2/bXVsZXhsIcgy7ceLe92s7v8vZsQG64hB",  // RPC URL for Polygon Mainnet
-      accounts: ["a5f1f59de420690d57900787a7bd61645479b3eed7f285384873d2a96da19555"],  // Hardcode your private key here
+      accounts: [""],  // Hardcode your private key here
     },
   },
   etherscan: {
